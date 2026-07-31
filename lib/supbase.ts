ceb41be3b76f893c95fe9fd032b89a5782a09726
ChessNeyshabur <chessneyshabur@example.com>
2026-08-01 04:19:32 +0430

    debug supabase env

--- lib/supbase.ts
+++ lib/supbase.ts
@@ -1,26 +1,55 @@
 import { createClient } from "@supabase/supabase-js";
  
  
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL;
  
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+ 
+const supabaseServiceKey =
+  process.env.SUPABASE_SERVICE_ROLE_KEY;
+ 
+ 
+ 
+console.log(
+  "SUPABASE URL:",
+  supabaseUrl ? "FOUND" : "MISSING"
+);
+ 
+ 
+console.log(
+  "SUPABASE SERVICE KEY:",
+  supabaseServiceKey ? "FOUND" : "MISSING"
+);
+ 
  
  
 if (!supabaseUrl || !supabaseServiceKey) {
+ 
   throw new Error(
     "Supabase environment variables are missing"
   );
+ 
 }
  
  
+ 
 export const supabase = createClient(
+ 
   supabaseUrl,
+ 
   supabaseServiceKey,
+ 
   {
+ 
     auth: {
+ 
       autoRefreshToken: false,
+ 
       persistSession: false,
+ 
     },
+ 
   }
+ 
 );
  
