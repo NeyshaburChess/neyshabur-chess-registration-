6a5600ba87c3a9b7e1f77cdb791d3f1345f55f50
ChessNeyshabur <chessneyshabur@example.com>
2026-08-01 07:00:02 +0430

    fix admin login production

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -1,58 +1,114 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcrypt";
+import { cookies } from "next/headers";
+ 
  
 export async function POST(request: Request) {
+ 
   try {
-    const { username, password } = await request.json();
  
-    console.log("LOGIN TRY:", username, password);
+    const {
+      username,
+      password
+    } = await request.json();
  
-    const admin = await prisma.admin.findUnique({
-      where: {
-        username: username
-      }
-    });
  
-    console.log("ADMIN FROM DB:", admin);
- 
-    if (!admin) {
-      return NextResponse.json({
-        step: "admin_not_found",
-        username
-      }, {
-        status: 401
-      });
+    if (!username || !password) {
+      return NextResponse.json(
+        {
+          error: "اطلاعات ناقص است"
+        },
+        {
+          status:400
+        }
+      );
     }
  
  
-    const check = await bcrypt.compare(
-      password,
-      admin.password
+    const admin = await prisma.admin.findUnique({
+      where:{
+        username: username.trim()
+      }
+    });
+ 
+ 
+    if (!admin) {
+ 
+      return NextResponse.json(
+        {
+          error:"ADMIN_NOT_FOUND"
+        },
+        {
+          status:401
+        }
+      );
+ 
+    }
+ 
+ 
+    const passwordCorrect =
+      await bcrypt.compare(
+        password,
+        admin.password
+      );
+ 
+ 
+    if (!passwordCorrect) {
+ 
+      return NextResponse.json(
+        {
+          error:"PASSWORD_WRONG"
+        },
+        {
+          status:401
+        }
+      );
+ 
+    }
+ 
+ 
+ 
+    const cookieStore = await cookies();
+ 
+ 
+    cookieStore.set(
+      "chess_admin_session",
+      String(admin.id),
+      {
+        httpOnly:true,
+        secure:true,
+        sameSite:"lax",
+        maxAge:60 * 60 * 24 * 7,
+        path:"/"
+      }
     );
  
-    console.log("PASSWORD CHECK:", check);
- 
  
     return NextResponse.json({
-      step: "success",
-      username: admin.username,
-      passwordMatch: check,
-      hash: admin.password
+      success:true
     });
  
  
-  } catch (error) {
+  } catch(error) {
  
-    console.error(error);
  
-    return NextResponse.json({
-      error: "server error",
-      detail: String(error)
-    },{
-      status:500
-    });
+    console.error(
+      "LOGIN ERROR:",
+      error
+    );
+ 
+ 
+    return NextResponse.json(
+      {
+        error:"SERVER_ERROR"
+      },
+      {
+        status:500
+      }
+    );
  
   }
+ 
 }
  
